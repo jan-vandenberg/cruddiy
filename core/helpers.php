28df00460f6ee7484c4a9f232e973e9386cc93cd
Germain Metti <germain@italic.fr>
2021-03-20 11:10:10 +0100

    retrieves and enhances postdata table keys and values on CREATE and UPDATE events

--- core/helpers.php
+++ core/helpers.php
@@ -1,10 +1,8 @@
 <?php
+// retrieves and enhances postdata table keys and values on CREATE and UPDATE events
 function parse_columns($table_name, $postdata) {
     global $link;
     $vars = array();
-
-    echo "<pre>";
-    print_r($postdata);
 
     // prepare a default return value
     $default = null;
@@ -34,21 +32,7 @@ function parse_columns($table_name, $postdata) {
         
         // check that fieldname was set before sending values to pdo
         $vars[$row['COLUMN_NAME']] = isset($_POST[$row['COLUMN_NAME']]) && $_POST[$row['COLUMN_NAME']] ? trim($_POST[$row['COLUMN_NAME']]) : $default;
-    }
-    
-    // /*
-    // debug
-    echo "<pre>";
-    print_r($postdata);
-    echo $row['COLUMN_NAME'] . "\t";
-    echo $row['DATA_TYPE'] . "\t";
-    echo $row['IS_NULLABLE'] . "\t";
-    echo $row['COLUMN_DEFAULT'] . "\t";
-    echo $row['EXTRA'] . "\t";
-    echo $default . "\n";
-    echo "</pre>";
-    // */
-    
+    }    
     return $vars;
 }
 ?>
