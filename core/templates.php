192a51d24d02a37a0364d84e42d002196a86f304
Germain Metti <germain@italic.fr>
2021-04-03 18:21:58 +0200

    edited post-generation text to reflect $keep_startpage option (same position as back buttons in generated pages)

--- core/templates.php
+++ core/templates.php
@@ -596,12 +596,12 @@ $startfile = <<<'EOT'
 </head>
 <body>
 <fieldset>
+<a href="../tables.php">Back to Available Tables</a>
 <center>
 <legend>Available CRUD pages</legend>
 <div class="form-group">
     {TABLE_BUTTONS}
 </div>
-<a href="../tables.php">Back to Available Tables</a>
 </center>
 </fieldset>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
