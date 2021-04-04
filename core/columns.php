ecfc986c357afc14940e008a1b2cb7946c45c49e
Germain Metti <germain@italic.fr>
2021-04-04 14:23:09 +0200

    ADDED check/uncheck all feature on columns selection page

--- core/columns.php
+++ core/columns.php
@@ -14,6 +14,13 @@
                 <div class="text-center">
                     <h4 class="mb-0">All available columns</h4>
                 </div>
+
+                <div class="col-md-4 offset-md-8">
+                    <input type="checkbox" id="checkall">
+                    <label for="checkall">Check/uncheck all</label>
+                </div>
+
+
                 <form class="form-horizontal" action="generate.php" method="post">
                     <fieldset>
                         <?php
@@ -154,5 +161,13 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script>
+$(document).ready(function () {
+    $('#checkall').click(function(e) {
+        var chb = $('.form-horizontal').find('input[type="checkbox"]');
+        chb.prop('checked', !chb.prop('checked'));
+    });
+});
+</script>
 </body>
 </html>
