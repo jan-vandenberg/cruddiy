d312ddb9d7b18200706b8f3d11e424887fcf2792
Max Resnikoff <maxresnikoff@gmail.com>
2021-06-08 20:42:02 +0100

    Added reference to existing config variables
    - Added the inclusion of config.php if the file exists
    - Referenced config.php variables in each form field
    - Removed random spacings in html code

--- core/index.php
+++ core/index.php
@@ -1,12 +1,18 @@
+<?php
+if (file_exists("../config.php")) {
+    include("../config.php");
+}
+?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Connect to database</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <style type="text/css">
-                table tr td:last-child a{
+        table tr td:last-child a {
             margin-right: 15px;
         }
     </style>
@@ -14,7 +20,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-4 mx-auto">
-            <form class="form-group-row" action="relations.php" method="post" >
+            <form class="form-group-row" action="relations.php" method="post">
                 <fieldset>
 
                     <!-- Form Name -->
@@ -25,36 +31,36 @@
                     <!-- Text input-->
                     <div class="form-group">
                         <label class="col-form-label" for="textinput">Server</label>
-                            <input id="server" name="server" type="text" placeholder="localhost" class="form-control ">
+                        <input id="server" name="server" type="text" placeholder="localhost" class="form-control " value="<?php echo $db_server; ?>">
                     </div>
 
                     <!-- Text input-->
                     <div class="form-group">
                         <label class="col-form-label" for="textinput">Database</label>
-                            <input id="database" name="database" type="text" placeholder="" class="form-control input-md">
+                        <input id="database" name="database" type="text" placeholder="" class="form-control input-md" value="<?php echo $db_name; ?>">
                     </div>
                     <!-- Text input-->
                     <div class="form-group">
                         <label class="col-form-label" for="textinput">Username</label>
-                            <input id="username" name="username" type="text" placeholder="" class="form-control input-md">
+                        <input id="username" name="username" type="text" placeholder="" class="form-control input-md" value="<?php echo $db_user; ?>">
                     </div>
 
                     <!-- Password input-->
                     <div class="form-group">
                         <label class="col-form-label" for="passwordinput">Password</label>
-                            <input id="password" name="password" type="password" placeholder="" class="form-control input-md">
+                        <input id="password" name="password" type="password" placeholder="" class="form-control input-md" value="<?php echo $db_password; ?>">
                     </div>
 
                     <!-- Number records per page-->
                     <div class="form-group">
-                            <label class="col-form-label" for="recordsperpage">Items per generated page</label>
-                            <input id="numrecordsperpage" name="numrecordsperpage" type="number" min="1" max="1000" placeholder="Number of items per page" class="form-control input-md" value="10">
+                        <label class="col-form-label" for="recordsperpage">Items per generated page</label>
+                        <input id="numrecordsperpage" name="numrecordsperpage" type="number" min="1" max="1000" placeholder="Number of items per page" class="form-control input-md" value="10" value="<?php echo $no_of_records_per_page ?>">
                     </div>
 
 
                     <div class="form-group">
                         <label class="col-form-label" for="index"></label>
-                            <button id="index" name="index" class="btn btn-primary">Submit</button>
+                        <button id="index" name="index" class="btn btn-primary">Submit</button>
                     </div>
                 </fieldset>
             </form>
@@ -65,4 +71,5 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
+
 </html>
