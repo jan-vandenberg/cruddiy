5cbf019f0dbdb5c45228a5eebb1982d76d7c075f
Germain Metti <germain@italic.fr>
2023-11-26 12:49:14 +0400

    IN (Hindi) translation (Deepl)

---
+++ core/locales/in.php
@@ -0,0 +1,47 @@
+<?php
+return array(
+    // menus
+    'Select Page' => 'पेज का चयन करें',
+    'Back' => 'वापस',
+    'Back to List' => 'सूची में वापस जाएं',
+
+    // list records
+    'View' => 'देखें',
+    'Reset View' => 'दृश्य रीसेट करें',
+    'View Record' => 'रिकॉर्ड देखें',
+    '%s Details' => '%s का विवरण',
+    'Actions' => 'कार्य',
+    'No records were found.' => 'कोई रिकॉर्ड नहीं मिले।',
+
+    // update records
+    'Update Record' => 'रिकॉर्ड अपडेट करें',
+    'update_record_instructions' => 'कृपया इनपुट मानों को संपादित करें और रिकॉर्ड को अपडेट करने के लिए सबमिट करें।',
+
+    // delete records
+    'Delete Record' => 'रिकॉर्ड हटाएं',
+    'delete_record_confirm' => 'क्या आप वाकई इस रिकॉर्ड को हटाना चाहते हैं?',
+
+    // add records
+    'Add New Record' => 'नया रिकॉर्ड जोड़ें',
+    'add_new_record_instructions' => 'कृपया इस फॉर्म को भरें और डेटाबेस में रिकॉर्ड जोड़ने के लिए सबमिट करें।',
+    'Create' => 'बनाएँ',
+    'required_fiels_instructions' => '* फ़ील्ड को खाली नहीं छोड़ा जा सकता',
+
+    // search & paging
+    'Search this table' => 'इस तालिका को खोजें',
+    'First' => 'पहला',
+    'Last' => 'अंतिम',
+
+    // buttons
+    'Yes' => 'हाँ',
+    'No' => 'नहीं',
+    'Cancel' => 'रद्द करें',
+
+    // errors
+    'Error' => 'त्रुटि',
+    'Invalid Request' => 'अमान्य अनुरोध',
+    'stmt_error' => 'उफ़! कुछ गलत हो गया। कृपया बाद में पुनः प्रयास करें।',
+    'unsupported_no_pk' => 'प्राथमिक कुंजी के बिना टेबल का संपादन अभी तक समर्थित नहीं है।',
+    'invalid_request_instructions' => 'क्षमा करें, आपने एक अमान्य अनुरोध किया है। कृपया <a href="index.php" class="alert-link">वापस जाएँ</a> और पुनः प्रयास करें।'
+);
+?>
