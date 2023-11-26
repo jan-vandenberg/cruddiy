e0ec6a7f3c3a28b55c6c1319dd9796165f72a25a
Germain Metti <germain@italic.fr>
2023-11-26 09:05:24 +0400

    added link to index page in navbar

--- core/templates.php
+++ core/templates.php
@@ -629,7 +629,7 @@ EOT;
 
 $navbarfile = <<<'EOT'
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand nav-link disabled" href="#">{APP_NAME}</a>
+  <a class="navbar-brand nav-link" href="index.php">{APP_NAME}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
