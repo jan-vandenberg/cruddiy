33bf6a60a701c7ad0e12d293fe8bc136685b3ece
Germain Metti <germain@italic.fr>
2023-11-26 12:42:24 +0400

    ES translation (Deepl)

---
+++ core/locales/es.php
@@ -0,0 +1,47 @@
+<?php
+return array(
+    // menus
+    'Select Page'                  => 'Seleccionar página',
+    'Back'                         => 'Atrás',
+    'Back to List'                 => 'Volver a la lista',
+
+    // list records
+    'View'                         => 'Ver',
+    'Reset View'                   => 'Restablecer vista',
+    'View Record'                  => 'Ver registro',
+    '%s Details'                   => 'Detalles de %s',
+    'Actions'                      => 'Acciones',
+    'No records were found.'       => 'No se encontraron registros.',
+
+    // update records
+    'Update Record'                => 'Actualizar registro',
+    'update_record_instructions'   => 'Edite los valores y envíe para actualizar el registro.',
+
+    // delete records
+    'Delete Record'                => 'Eliminar registro',
+    'delete_record_confirm'        => '¿Está seguro de que desea eliminar este registro?',
+
+    // add records
+    'Add New Record'               => 'Añadir nuevo registro',
+    'add_new_record_instructions'  => 'Complete este formulario y envíe para añadir un registro a la base de datos.',
+    'Create'                       => 'Crear',
+    'required_fiels_instructions'  => '* campo requerido',
+
+    // search & paging
+    'Search this table'            => 'Buscar en esta tabla',
+    'First'                        => 'Primero',
+    'Last'                         => 'Último',
+
+    // buttons
+    'Yes'                          => 'Sí',
+    'No'                           => 'No',
+    'Cancel'                       => 'Cancelar',
+
+    // errors
+    'Error'                        => 'Error',
+    'Invalid Request'              => 'Solicitud inválida',
+    'stmt_error'                   => '¡Vaya! Algo salió mal. Intente nuevamente más tarde.',
+    'unsupported_no_pk'            => 'Editar tablas sin clave primaria aún no es compatible.',
+    'invalid_request_instructions' => 'Lo siento, ha realizado una solicitud inválida. Por favor, <a href="index.php" class="alert-link">regrese</a> e intente nuevamente.',
+);
+?>
