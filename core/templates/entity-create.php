5a7e1b5445534ddeef0eb6514bc67049a6ee8c0f
Germain Metti <germain@italic.fr>
2023-12-02 00:55:43 +0400

    handle the file uploads on the Create form and save the name to database

--- core/templates/entity-create.php
+++ core/templates/entity-create.php
@@ -4,20 +4,58 @@ require_once('helpers.php');
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    {CREATE_POST_VARIABLES}
 
-    $stmt = $link->prepare("INSERT INTO `{TABLE_NAME}` ({CREATE_COLUMN_NAMES}) VALUES ({CREATE_QUESTIONMARK_PARAMS})");
-
-    try {
-        $stmt->execute([ {CREATE_SQL_PARAMS} ]);
-    } catch (Exception $e) {
-        error_log($e->getMessage());
-        $error = $e->getMessage();
+    // Checking for upload fields
+    $upload_results = array();
+    if (!empty($_FILES)) {
+        foreach ($_FILES as $key => $value) {
+            // Check if the file was actually uploaded
+            if ($value['error'] != UPLOAD_ERR_NO_FILE) {
+                // echo "Field " . $key . " is a file upload.\n";
+                $this_upload = handleFileUpload($_FILES[$key]);
+                $upload_results[] = $this_upload;
+                // Put the filename in the POST data to save it in DB
+                if (!in_array(true, array_column($this_upload, 'error')) && !array_key_exists('error', $this_upload)) {
+                    $_POST[$key] = $this_upload['success'];
+                }
+            }
+        }
     }
 
-    if (!isset($error)){
-        $new_id = mysqli_insert_id($link);
-        header("location: {TABLE_NAME}-read.php?{COLUMN_ID}=$new_id");
+    $upload_errors = array();
+    foreach ($upload_results as $result) {
+        if (isset($result['error'])) {
+            $upload_errors[] = $result['error'];
+        }
+    }
+
+    // Check for regular fields
+    if (!in_array(true, array_column($upload_results, 'error'))) {
+
+        {CREATE_POST_VARIABLES}
+
+
+        $stmt = $link->prepare("INSERT INTO `{TABLE_NAME}` ({CREATE_COLUMN_NAMES}) VALUES ({CREATE_QUESTIONMARK_PARAMS})");
+
+        try {
+            $stmt->execute([ {CREATE_SQL_PARAMS} ]);
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            $error = $e->getMessage();
+        }
+
+        if (!isset($error)){
+            $new_id = mysqli_insert_id($link);
+            header("location: {TABLE_NAME}-read.php?{COLUMN_ID}=$new_id");
+        } else {
+            $uploaded_files = array();
+            foreach ($upload_results as $result) {
+                if (isset($result['success'])) {
+                    // Delete the uploaded files if there were any error while saving postdata in DB
+                    unlink($upload_target_dir . $result['success']);
+                }
+            }
+        }
     }
 }
 ?>
@@ -38,6 +76,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     <div class="page-header">
                         <h2><?php translate('Add New Record') ?></h2>
                     </div>
+                    <?php print_error_if_exists(@$upload_errors); ?>
                     <?php print_error_if_exists(@$error); ?>
                     <p><?php translate('add_new_record_instructions') ?></p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
