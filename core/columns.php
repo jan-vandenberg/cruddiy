b5f9d888a4e1f93689fbb3c7f924c20408e39958
Germain Metti <germain@italic.fr>
2023-12-05 05:00:37 +0400

    fix too long column names on All Columns page

--- core/columns.php
+++ core/columns.php
@@ -283,7 +283,7 @@ function is_table_referenced($table_name) {
                                         ?>
 
                                         <label class="col-form-label <?= htmlspecialchars($labelClassString) ?>" for="<?= htmlspecialchars($table['name']) . '-' . $i ?>">
-                                            <abbr title="<?= htmlspecialchars($column['name']) ?>, <?php echo $column['type'] ?>"><?= truncate(htmlspecialchars($column['name']), 25) ?></abbr>
+                                            <abbr title="<?= htmlspecialchars($column['name']) ?>, <?php echo $column['type'] ?>"><?= truncate(htmlspecialchars($column['name']), 20) ?></abbr>
 
                                             <span title="<?= htmlspecialchars($labelClassString) ?>"><?= implode(' ', $labelAttributes['emojis']) ?></span>
                                         </label>
