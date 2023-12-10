55cc3f5ebc50f54c3c2475e321a198d66acf1ed5
Germain Metti <germain@italic.fr>
2023-12-10 09:07:58 +0400

    "my apps" new menu on homepage

--- core/index.php
+++ core/index.php
@@ -33,9 +33,24 @@ if (isset($_GET['generator']) && $_GET['generator'] == 'new') {
     <div class="row">
         <div class="col-md-4 mx-auto">
 
+
+        <?php
+        // Look for previously generated apps in core/
+        $configDirectories = getConfigDirectories(__DIR__);
+        if (!empty($configDirectories)) :
+            ?>
+            <div class="text-center pt-5">
+                <h4>My apps</h4>
+                <?php foreach($configDirectories as $dir) : ?>
+                    <a href="<?php echo htmlspecialchars($dir); ?>" target="_blank" rel="noopener noreferrer" class="btn btn-primary my-2 mx-2"><?php echo htmlspecialchars($dir); ?> &nbsp; <i class="fa fa-external-link" aria-hidden="true"></i></a>
+                <?php endforeach ?>
+            </div>
+        <?php endif ?>
+
             <!-- Form Name -->
             <div class="text-center pt-5">
-                <h4>Enter database information</h4>
+                <h4>Generate a new app</h4>
+                <h6>Enter database information:</h6>
             </div>
 
             <!-- Error messages -->
@@ -144,6 +159,7 @@ if (isset($_GET['generator']) && $_GET['generator'] == 'new') {
         </div>
     </div>
 </div>
+<script src="https://kit.fontawesome.com/6b773fe9e4.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
